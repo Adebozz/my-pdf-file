1d8aa0c72c000c6384185f3b766789f01f6e4184
Adeboss <ademolaadeigbe3@gmail.com>
2025-07-17 23:54:26 +0100

    `Added a link to the PDFPreview component`

--- components/PDFPreview.tsx
+++ components/PDFPreview.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const PDFPreview = () => {
   return (
     <div>
-      This is my pdf file.
+      This is my pdf file. 
+      <a href="http://">Click Me </a>
     </div>
   )
 }
